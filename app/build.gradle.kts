256997915e483a7b58f918fb1ee3c20ad6700909
dragon5354 <dragon5354@naver.com>
2023-09-30 17:45:29 +0900

    라이브러리, 권한, 구글지도(임시)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,11 @@ android {
     namespace = "com.example.seoultoilet"
     compileSdk = 33
 
+    // 데이터 바인딩
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.seoultoilet"
         minSdk = 26
@@ -44,4 +49,15 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // 구글 맵 관련 라이브러리
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    // 구글 맵 클러스터를 위한 라이브러리
+    implementation ("com.google.maps.android:android-maps-utils:2.3.0")
+
+    // 책에 나온 라이브러리가 좀 오래된 버전이라 아래 2개는 X에서 지원하는 걸로 마이그레이션 했음.
+
+    // 맵 서치 바 카드를 만들기 위한 CardView 라이브러리
+    implementation("androidx.cardview:cardview:1.0.0")
+    // 현재위치 버튼에서 사용하는 FloatingActionButton을 위한 라이브러리
+    implementation("com.google.android.material:material:1.0.0")
 }
--- app/src/main/java/com/example/seoultoilet/MainActivity.kt
+++ app/src/main/java/com/example/seoultoilet/MainActivity.kt
@@ -1,11 +1,113 @@
 package com.example.seoultoilet
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.seoultoilet.databinding.ActivityMainBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
+    // 뷰 바인딩
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var mapView: MapView
+    private lateinit var googleMap: GoogleMap
+    private var currentMarker: Marker? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // 바인딩
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        this.mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this@MainActivity)
     }
+
+    /**
+     * onMapReady()
+     * Map 이 사용할 준비가 되었을 때 호출
+     * @param googleMap
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+
+        currentMarker = setupMarker(LatLngEntity(37.5562,126.9724))  // default 서울역
+        currentMarker?.showInfoWindow()
+    }
+
+    /**
+     * setupMarker()
+     * 선택한 위치의 marker 표시
+     * @param locationLatLngEntity
+     * @return
+     */
+    private fun setupMarker(locationLatLngEntity: LatLngEntity): Marker? {
+
+        val positionLatLng = LatLng(locationLatLngEntity.latitude!!,locationLatLngEntity.longitude!!)
+        val markerOption = MarkerOptions().apply {
+            position(positionLatLng)
+            title("위치")
+            snippet("서울역 위치")
+        }
+
+        googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL  // 지도 유형 설정
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(positionLatLng, 15f))  // 카메라 이동
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15f))  // 줌의 정도 - 1 일 경우 세계지도 수준, 숫자가 커질 수록 상세지도가 표시됨
+        return googleMap.addMarker(markerOption)
+
+    }
+
+    // 하단부터 맵뷰의 라이프사이클 함수 호출을 위한 코드들
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+    override fun onDestroy() {
+        mapView.onDestroy()
+        super.onDestroy()
+    }
+
+
+    /**
+     * LatLngEntity data class
+     *
+     * @property latitude 위도 (ex. 37.5562)
+     * @property longitude 경도 (ex. 126.9724)
+     */
+    data class LatLngEntity(
+        var latitude: Double?,
+        var longitude: Double?
+    )
 }
